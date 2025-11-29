64feb00f67fc30e833ed69041650a1b3f4dd9f9a
Xenos <xenos@outlook.it>
2025-11-29 12:28:59 +0100

    commit n12

--- src/components/GoCatalogue.jsx
+++ src/components/GoCatalogue.jsx
@@ -9,7 +9,7 @@ export default function GoCatalogue() {
             <div className="flex flex-col lg:flex-row p-3 m-3 gap-10 lg:items-center">
                 <div className="w-1/2">
                     <h1 className="text-white text-3xl font-semibold">Sei in cerca di un account!?</h1>
-                    <h2 className="text-zinc-700 text-xl font-semibold capitalize">Esplora il catalogo di Xenos</h2>
+                    <h2 className="text-zinc-400 text-xl font-semibold capitalize">Esplora il catalogo di Xenos</h2>
                     <h2 className="text-white text-xl font-semibold capitalize">Clicca qui sotto</h2>
                     <Link to={'/full-access'}><button className="text-white p-3 px-6 mt-6 bg-blue-600 rounded-2xl cursor-pointer"><i className="fa-solid fa-angle-right bg-zinc-400/80 rounded-2xl px-1 py-0.5"></i> CATALOGO</button></Link>
                 </div>
